5e405b0cef8f64317d01add84f48f802ba6198d7
Andrew Mason <andrew@nocturnal.net.au>
2014-11-24 23:02:13 +1030

    Enty which imports some default traits to provide x2xml methods

--- src/Timber/Entity.php
+++ src/Timber/Entity.php
@@ -1,17 +1,33 @@
 <?php
 namespace Timber;
-use Timber\XML\DOMDocument;
+use \Timber\XML\DOMDocument;
+use \Timber\EntityInterface;
 
-class Entity implements \JsonSerializable
+class Entity implements \JsonSerializable, EntityInterface
 {
     public $content;
-    public $ns = 'urn:Timber:Entity:Foo';
+    public $ns   = 'urn:Timber:Entity';
+    public $name = 'Entity';
     
     use \Timber\Utils\Array2XMLTrait;
+    use \Timber\Utils\Object2XMLTrait;
     
     public function __construct($content = null)
     {
         $this->content = $content;
+        $className     = get_class($this);
+        $this->name    = substr($className, strripos($className, '\\', -1) + 1);
+    }
+    
+    
+    public function getNS()
+    {
+        return $this->ns;
+    }
+    
+    public function getName()
+    {
+        return $this->name;
     }
     
     /**
@@ -32,10 +48,14 @@ class Entity implements \JsonSerializable
     public function __toXML()
     {
         $dom = new DOMDocument();
+        $el = $dom->appendChild($dom->createElementNS($this->ns, str_replace('\\', ':', $this->name)));
+        
         if (is_string($this->content)) {
             $dom->appendChild($dom->createElementNS($this->ns,'string', $this->content));
         } elseif(is_array($this->content)) {
-            $this->Array2XML($dom, $this->content);
+            $this->Array2XML($el, $this->content);
+        } else if (is_object($this->content)) {
+            $this->object2XML($el, $this->content);
         }
         
         return $dom->saveXML($dom->documentElement);
