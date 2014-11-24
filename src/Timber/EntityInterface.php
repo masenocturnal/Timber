94c1fd0f8062846ce1441e18905ef449299501af
Andrew Mason <andrew@nocturnal.net.au>
2014-11-24 23:02:38 +1030

    Contract for the Entity

---
+++ src/Timber/EntityInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace Timber;
+
+interface EntityInterface
+{
+    
+    public function getName();
+    public function getNS();    
+    public function object2XML($node, $obj);
+    public function array2XML(\DOMNode $node, array $arr);
+
+}
