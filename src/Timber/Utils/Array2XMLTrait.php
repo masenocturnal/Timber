5e5cf5f8b12b7c6ce0aa0dac51196c4d3f045a13
Andrew Mason <andrew@nocturnal.net.au>
2015-01-31 01:10:42 +1030

    allow an override on the key

--- src/Timber/Utils/Array2XMLTrait.php
+++ src/Timber/Utils/Array2XMLTrait.php
@@ -4,9 +4,11 @@ namespace Timber\Utils;
 trait Array2XMLTrait {
     public function array2XML(\DOMNode $node, array $arr) {
         $dom = $node->ownerDocument;
-        
-        foreach ($arr as $k=>$v) {
-            if (is_array($v)) {
+        foreach ($arr as $k => $v) {
+            if ($this->_format != null &&
+                isset($this->_format[$k])) {
+                    $this->_format[$k]($node, $k, $v);
+            } elseif (is_array($v)) {
                 $newNode = $node->appendChild($dom->createElementNS($this->ns, $k));
                 $this->array2XML($newNode, $v);
             } else {
--- src/Timber/Utils/Object2XMLTrait.php
+++ src/Timber/Utils/Object2XMLTrait.php
@@ -2,11 +2,15 @@
 namespace Timber\Utils;
 
 trait Object2XMLTrait {
-    public function object2XML($node, $obj) {        
+    public function object2XML($node, $obj) 
+    {
         $dom = $node->ownerDocument;
         
-        foreach (get_object_vars($obj) as $k=>$v) {
-            if (is_object($v)) {
+        foreach (get_object_vars($obj) as $k => $v) {
+            if ($this->_format != null &&
+                isset($this->_format[$k])) {
+                    $this->_format[$k]($node, $k, $v);
+            } elseif (is_object($v)) {
                 $newNode = $node->appendChild($dom->createElement($this->ns, $k));
                 $this->object2XML($newNode, $v);
             } else {
