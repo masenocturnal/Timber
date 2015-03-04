d938bbf44cf88cf94fb072180f7fcf642ccb2b3a
Andrew Mason <andrew@nocturnal.net.au>
2015-03-05 08:05:26 +1030

    Rather than looking for a closure in an array (which can be slow) we instead establish a convention of field_nameFormat that it will look for in order to format a particular field

--- src/Timber/Utils/Array2XMLTrait.php
+++ src/Timber/Utils/Array2XMLTrait.php
@@ -5,9 +5,9 @@ trait Array2XMLTrait {
     public function array2XML(\DOMNode $node, array $arr) {
         $dom = $node->ownerDocument;
         foreach ($arr as $k => $v) {
-            if ($this->_format != null &&
-                isset($this->_format[$k])) {
-                    $this->_format[$k]($node, $k, $v);
+            $name = $k.'Format';
+            if (method_exists($this, $name)) {
+                $this->$name($node, $k, $v);
             } elseif (is_array($v)) {
                 $newNode = $node->appendChild($dom->createElementNS($this->ns, $k));
                 $this->array2XML($newNode, $v);
