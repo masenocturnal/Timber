b0c2b4e1244e96e59bb565d6401722636ea6853c
Andrew Mason <andrew@nocturnal.net.au>
2015-08-17 00:12:00 +0930

    Allow args to be passed in via XSLT

--- src/Timber/URL/ReverseURLMapper.php
+++ src/Timber/URL/ReverseURLMapper.php
@@ -11,27 +11,44 @@ class ReverseURLMapper
 {
     public static $url;
     
-    public function __construct(URL $url)
+    public function __construct(Url $url)
     {
         self::$url = $url;
     }
     
-    public static function map($id, array $params = null)
+    public static function map($id, $args = null)
     {
-        $url = self::$url;
-        $str = '';
-        if (!empty($id)) {
-            try {
-                $str = $url->get(array(
-                    'for' => strval($id),
-                    'year' => 2012,
-                    'month' => '01',
-                    'title' => 'some-blog-post'
-                ));
-            } catch (\Exception $e) {
-                //error_log($e->getMessage());
+        $url    = self::$url;
+        $params = [];
+        $str    = '';
+
+        if ($args == null) {
+            $params = [];
+        } else {
+
+            mb_parse_str($args, $params);
+
+            error_log("HERE ");
+
+            // for is reserved for the $id
+            if (isset($params['for'])) {
+                unset($params['for']);
             }
-        }        
+        }
+        if (!empty($id)) {
+
+            $params['for'] = $id;
+            if($id == 'getProduct') {
+                error_log("ID ".$id);
+                error_log("STRING ".print_r($params, true));
+            }
+
+            try {
+                $str = $url->get($params);
+            } catch (\Exception $e) {
+                error_log($e->getMessage());
+            }
+        }
         return $str;
     }
 
