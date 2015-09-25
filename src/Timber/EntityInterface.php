0b2979c7ae6e6c6aa5c01e2165f1324c64744401
Andrew Mason <andrew@nocturnal.net.au>
2015-09-25 17:42:40 +0930

    add setLogger to te interface so that we can log

--- src/Timber/EntityInterface.php
+++ src/Timber/EntityInterface.php
@@ -7,6 +7,7 @@ interface EntityInterface
     public function getName();
     public function getNS(); 
     public function __toXML();
+    public function setLogger($logger);
     //public function object2XML(&$node, &$obj);
    // public function array2XML(\DOMNode $node, $arr);
 
